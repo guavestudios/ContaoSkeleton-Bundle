3cf800d449adb46acfdb73746c3e6750c9e28ed6
Christopher Stadler <christopher.stadler@guavestudios.com>
2024-03-28 21:28:19 +0100

    feature: add and run test system

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Guave\ContaoSkeletonBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
--- src/DependencyInjection/GuaveContaoSkeletonExtension.php
+++ src/DependencyInjection/GuaveContaoSkeletonExtension.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Guave\ContaoSkeletonBundle\DependencyInjection;
 
 use Exception;
